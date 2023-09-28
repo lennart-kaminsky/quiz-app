c3b4f46a7858ee0584d54a254f7ed231f738e875
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2023-09-28 14:30:03 +0200

    add darkmode

--- js/index.js
+++ js/index.js
@@ -15,3 +15,25 @@ for (let i = 0; i < answerButton.length; i++) {
     }
   });
 }
+
+//Dark Mode
+const darkmodeButton = document.querySelector('[data-js="checkbox--darkmode"]');
+let rootElement = document.documentElement;
+//App starts in DarkMode with checkbox checked
+let darkmode = true; //used instead of (darkmodeButton.checked) because page always gets reloaded
+
+darkmodeButton.addEventListener("change", () => {
+  if (!darkmode) {
+    rootElement.style.setProperty("--color-white", "#f0ebeb");
+    rootElement.style.setProperty("--first-background-color", "#121111");
+    rootElement.style.setProperty("--second-background-color", "#282525");
+    darkmode = true;
+  } else {
+    rootElement.style.setProperty("--color-white", "#121111");
+    rootElement.style.setProperty("--first-background-color", "#bebaba");
+    rootElement.style.setProperty("--second-background-color", "#f0ebeb");
+    darkmode = false;
+  }
+});
+
+// #7db5cf
